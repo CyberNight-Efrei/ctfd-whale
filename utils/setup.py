daba7aaa48a38b480c99ebd0c60ac80bfe05b9e2
Q <quentin.vincent@efrei.net>
2025-10-14 22:50:08 +0200

    fix(setup.py): fix template

--- utils/setup.py
+++ utils/setup.py
@@ -62,7 +62,7 @@ use_compression = true
         'ssh {{ container.user}}@{{ get_config("whale:frp_direct_ip_address", "127.0.0.1") }} -p {{ container.port }}',
         '''
 [ssh_{{ container.user_id|string }}-{{ container.uuid }}]
-type = ssh
+type = tcp
 local_ip = {{ container.user_id|string }}-{{ container.uuid }}
 local_port = {{ container.challenge.redirect_port }}
 remote_port = {{ container.port }}
